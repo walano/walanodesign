18f8fe6129d735970927af6d175ff3cc22e4d27a
Walano <hermanemubiala@gmail.com>
2026-03-25 14:53:35 +0100

    fix: use CompressedStaticFilesStorage to avoid manifest errors

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -103,7 +103,7 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
